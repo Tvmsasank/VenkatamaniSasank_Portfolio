0a6b63a89e042fb17c6d887216c69d4b701a885d
ICFAI <ICFAI@DESKTOP-1R2NMOR>
2026-05-06 16:31:06 +0530

    Fixed GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/VenkatamaniSasank_Portfolio/',
+  base: '/my-portfolio/',
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  assetsInclude: ['**/*.svg', '**/*.csv'],
 })
